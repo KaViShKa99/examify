171b154da9336c0e5396e400f00c115125d86ad7
kavishka <kaviya1234z@gmail.com>
2023-04-10 21:02:09 +0530

    update home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PapersComponent } from './papers/papers.component';
+import { PaperDetailComponent } from './papers/paper-detail/paper-detail.component';
+import { QuestionListComponent } from './papers/question-list/question-list.component';
+import { QuestionComponent } from './papers/question-list/question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PapersComponent,
+    PaperDetailComponent,
+    QuestionListComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
