e902e9f6fb321814676ac8931a5cbc3308d24ddd
kavishka <kaviya1234z@gmail.com>
2023-04-12 21:52:40 +0530

    added a question list with add button

--- client/src/app/papers/question-list/question-list.component.ts
+++ client/src/app/papers/question-list/question-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Question } from './question.model';
 
 @Component({
   selector: 'app-question-list',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class QuestionListComponent {
 
+  question : Question[] = [
+    new Question("question 1","description 1",["answer 1","answer 2","answer 3","answer 4"])
+  ] 
+ 
 }
--- client/src/app/papers/question-list/question/question.component.ts
+++ client/src/app/papers/question-list/question/question.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Question } from '../question.model'
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
+ @Input() q:any;
+ @Input() isLast: boolean = false;
+ @Input() question: any[] =[];
+ 
+ constructor(){
+  if(this.question.length == 0){
+    this.question.push(this.q)
+  }
+ }
+ 
+ addQuestion(){
+    this.question.push(new Question("question "+(this.question.length +1),"description "+(this.question.length +1),["answer 1","answer 2","answer 3","answer 4"]))
+ }
 
 }
